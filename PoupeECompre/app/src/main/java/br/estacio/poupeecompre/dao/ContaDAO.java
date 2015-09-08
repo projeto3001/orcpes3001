1db3fc3873edaf7d0d175a0be6b8b26e20b589ca
kachorroski <kernelkill@outlook.com>
2015-09-08 08:46:51 -0400

    ContaDAO, classe foi inicado o desenvolvimento e nao foi dado commit, atualizado e comitado.

--- PoupeECompre/app/src/main/java/br/estacio/poupeecompre/dao/ContaDAO.java
+++ PoupeECompre/app/src/main/java/br/estacio/poupeecompre/dao/ContaDAO.java
@@ -1,16 +1,13 @@
 package br.estacio.poupeecompre.dao;
 
-<<<<<<< HEAD
+
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import br.estacio.poupeecompre.dominio.Conta;
 
-/**
- * Created by kernelkill on 28/08/15.
- */
-=======
+
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
@@ -25,55 +22,33 @@ import java.util.List;
 import br.estacio.poupeecompre.dominio.Conta;
 import br.estacio.poupeecompre.dominio.Usuario;
 
->>>>>>> 4ed497f37812f07ed72730d4b1790c5a6b5d32db
+
 public class ContaDAO extends SQLiteOpenHelper {
     private static final String DATABASE = "PoupeCompre";
     private static final String TABLE = "Conta";
     private static final int VERSION = 1;
-<<<<<<< HEAD
-
-    public ContaDAO(Context context) {
-        super(context, DATABASE, null, VERSION);
-=======
     private Context context;
+
 
     public ContaDAO(Context context) {
         super(context, DATABASE, null, VERSION);
         this.context = context;
->>>>>>> 4ed497f37812f07ed72730d4b1790c5a6b5d32db
+
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-<<<<<<< HEAD
-        String sql = "CREATE TABLE" + TABLE +
-                "(id INTERGE PRIMARY KEY, " +
-                "descricao TEXT," +
-                "saldo NUMERIC NOT NULL," +
-                "usuarioId INTERGE NOT NULL," +
-=======
+
         String sql = "CREATE TABLE " + TABLE +
                 "(id INTEGER PRIMARY KEY, " +
                 "descricao TEXT," +
                 "saldo NUMERIC," +
->>>>>>> 4ed497f37812f07ed72730d4b1790c5a6b5d32db
+
                 "FOREIGN KEY(usuarioId) REFERENCES Usuario(id))";
         db.execSQL(sql);
     }
 
     @Override
-<<<<<<< HEAD
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
-    }
-
-    public Conta buscarPorId(Long contaId){
-        Conta conta = new Conta();
-
-
-
-
-=======
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
@@ -125,7 +100,6 @@ public class ContaDAO extends SQLiteOpenHelper {
                 BigDecimal.valueOf(c.getDouble(c.getColumnIndex("saldo"))),
                 usuario);
         conta.setId(c.getLong(c.getColumnIndex("id")));
->>>>>>> 4ed497f37812f07ed72730d4b1790c5a6b5d32db
         return conta;
     }
 }

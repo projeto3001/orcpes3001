3b473617dcfc8e0aa75fbcfe81b32f6e6fa07eb9
diegopaniago <diegopaniago1@gmail.com>
2015-10-23 18:51:57 -0300

    Refatorando o cadastro de receitas na tela

--- PoupeECompre/app/src/main/java/br/estacio/poupeecompre/activitys/CasdastroDeReceitasActivity.java
+++ PoupeECompre/app/src/main/java/br/estacio/poupeecompre/activitys/CasdastroDeReceitasActivity.java
@@ -5,14 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import br.estacio.poupeecompre.R;
 
 public class CasdastroDeReceitasActivity extends AppCompatActivity {
+
+    private void cadastrarReceita(){
+        //TODO: Fazer apos o cadastro de contas estiver completo
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_casdastro_de_receitas);
+        final Button cadastrar = (Button) findViewById(R.id.receitasBtn);
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cadastrarReceita();
+            }
+        });
     }
 
     @Override

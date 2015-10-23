c644337d949882e00585f0646e137408d9d4c360
kachorroski <kernelkill@outlook.com>
2015-10-23 20:32:07 -0300

    Tela de categorias e activity

--- PoupeECompre/app/src/main/java/br/estacio/poupeecompre/activitys/CadastroDeCategoriaActivity.java
+++ PoupeECompre/app/src/main/java/br/estacio/poupeecompre/activitys/CadastroDeCategoriaActivity.java
@@ -1,11 +1,9 @@
 package br.estacio.poupeecompre.activitys;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import br.estacio.poupeecompre.R;
+
 
 public class CadastroDeCategoriaActivity extends AppCompatActivity {
 
@@ -14,15 +12,6 @@ public class CadastroDeCategoriaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_de_categoria);
 
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
--- PoupeECompre/app/src/main/java/br/estacio/poupeecompre/activitys/CadastroDeContaActivity.java
+++ PoupeECompre/app/src/main/java/br/estacio/poupeecompre/activitys/CadastroDeContaActivity.java
@@ -1,19 +1,39 @@
 package br.estacio.poupeecompre.activitys;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import br.estacio.poupeecompre.R;
+import br.estacio.poupeecompre.dao.ContaDAO;
 
 public class CadastroDeContaActivity extends AppCompatActivity {
+
+    public void cadastrarDespesa(){
+        EditText descricao, usuario, saldo;
+        descricao = (EditText) findViewById(R.id.desc_conta);
+        usuario = (EditText) findViewById(R.id.usuario_conta);
+        saldo = (EditText) findViewById(R.id.saldo_conta);
+
+        ContaDAO contaDAO = new ContaDAO(this);
+
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_de_conta);
+        final Button cadastrar = (Button) findViewById(R.id.btnConta);
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cadastrarDespesa();
+            }
+        });
 
 
     }

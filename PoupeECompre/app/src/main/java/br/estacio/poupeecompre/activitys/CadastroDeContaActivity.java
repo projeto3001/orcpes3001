f0a501c9d9ed20151516ff4d7c5b086cdf74f17f
kachorroski <kernelkill@outlook.com>
2015-10-23 19:49:48 -0300

    Mexendo nas coisas aqui

--- PoupeECompre/app/src/main/java/br/estacio/poupeecompre/activitys/CadastroDeContaActivity.java
+++ PoupeECompre/app/src/main/java/br/estacio/poupeecompre/activitys/CadastroDeContaActivity.java
@@ -16,16 +16,6 @@ public class CadastroDeContaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cadastro_de_conta);
 
 
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
